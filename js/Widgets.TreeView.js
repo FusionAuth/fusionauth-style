e0521a009ebc880ca7e039876044e30cf6359877
Brian Pontarelli <brian@inversoft.com>
2017-05-12 22:14:41 -0600

    Alerts, charts, etc

--- js/Widgets.TreeView.js
+++ js/Widgets.TreeView.js
@@ -18,9 +18,7 @@ Prime.Widgets = Prime.Widgets || {};
 Prime.Widgets.TreeView = function(element) {
   this.element = Prime.Document.Element.wrap(element);
   Prime.Utils.bindAll(this);
-  this._setInitialOptions();
-
-  this.element.query('a.folder-toggle').each(function(e) {
+  this.element.query('a.folder-toggle').each(function (e) {
     e.addEventListener('click', this._handleClick);
   }.bind(this));
 };
@@ -28,26 +26,6 @@ Prime.Widgets.TreeView = function(element) {
 Prime.Widgets.TreeView.constructor = Prime.Widgets.TreeView;
 
 Prime.Widgets.TreeView.prototype = {
-  /**
-   * Set more than one option at a time by providing a map of key value pairs. This is considered an advanced
-   * method to set options on the widget. The caller needs to know what properties are valid in the options object.
-   *
-   * @param {Object} options Key value pair of configuration options.
-   * @returns {Prime.Widgets.TreeView} This TreeView.
-   */
-  withOptions: function(options) {
-    if (!Prime.Utils.isDefined(options)) {
-      return this;
-    }
-
-    for (var option in options) {
-      if (options.hasOwnProperty(option)) {
-        this.options[option] = options[option];
-      }
-    }
-    return this;
-  },
-
   /* ===================================================================================================================
    * Private methods
    * ===================================================================================================================*/
@@ -66,24 +44,5 @@ Prime.Widgets.TreeView.prototype = {
       a.addClass('active');
       li.addClass('active');
     }
-  },
-
-  /**
-   * Set the initial options for this widget.
-   * @private
-   */
-  _setInitialOptions: function() {
-    // Defaults
-    this.options = {
-      'dateOnly': false
-    };
-
-    var userOptions = Prime.Utils.dataSetToOptions(this.element);
-    for (var option in userOptions) {
-      if (userOptions.hasOwnProperty(option)) {
-        this.options[option] = userOptions[option];
-      }
-    }
   }
 };
-
